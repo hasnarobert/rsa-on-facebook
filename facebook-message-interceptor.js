14ed267ff560ddeb36627d0aa6d514584311d7fb
Robert Hasna <rhasna@adobe.com>
2015-10-08 21:24:52 +0300

    Message in facebook chat intercepted and altered.

---
+++ facebook-message-interceptor.js
@@ -0,0 +1,44 @@
+function injectScript(source) {
+    var elem = document.createElement("script");
+    elem.type = "text/javascript";
+    elem.innerHTML = source;
+    document.documentElement.appendChild(elem);
+}
+
+function setupXMLHttpRequestInterceptor() {
+    var isChatMessage = false;
+
+    var originalOpen = window.XMLHttpRequest.prototype.open;
+
+    function proxyOpen(method, path, async) {
+        if (method == "POST" && path == "/ajax/mercury/send_messages.php") {
+            isChatMessage = true;
+        }
+
+        return originalOpen.apply(this, [].slice.call(arguments));
+    }
+
+    window.XMLHttpRequest.prototype.open = proxyOpen;
+
+    var originalSend = window.XMLHttpRequest.prototype.send;
+
+    function proxySend(method, path, async) {
+        if (isChatMessage == true) {
+            var messagePayload = arguments[0];
+            var startOfMessage = 7 + messagePayload.indexOf("[body]");
+            var endOfMessage = messagePayload.indexOf("&", startOfMessage);
+            var message = messagePayload.substring(startOfMessage, endOfMessage);
+            console.log(message);
+            isChatMessage = false;
+
+            arguments[0] = messagePayload.substring(0, startOfMessage) + "muhahaha" + messagePayload.substring(endOfMessage);
+        }
+
+        return originalSend.apply(this, [].slice.call(arguments));
+    }
+
+    window.XMLHttpRequest.prototype.send = proxySend;
+
+}
+
+injectScript("(" + setupXMLHttpRequestInterceptor.toString() + ")()");
